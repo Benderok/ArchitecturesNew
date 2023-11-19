c274194a096d316e8156dc73e6b464aeb6c76859
valentinpanferin <vill451@gmail.com>
2023-11-19 23:40:12 +0400

    seminar 3

---
+++ src/Seminar_3/ulmExample/CalculatorView.java
@@ -0,0 +1,10 @@
+package Seminar_3.ulmExample;
+
+import java.util.Observer;
+
+// Интерфейс для View
+interface CalculatorView {
+    void setObserver(ViewObserver observer);
+    void updateResult(double result);
+    void clearScreen();
+}
